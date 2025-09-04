8a7fd416153a7e561d825183b6e1a419dae52d75
ersa-mezuraj9 <ersamezuraj9@gmail.com>
2025-09-04 13:58:36 +0200

    Implement word counting functions and book reading utility

---
+++ python_case_studies/language_jpo.py
@@ -0,0 +1,94 @@
+text = "This is my test text. We're keeping this text short to keep things manageable."
+
+from collections import Counter
+
+def count_words(text):
+    """
+    Count the number of times each word occurs in text(str). Return dictionary where keys are unique and values are word counts. Skip punctuation. For this function we have used for loop.
+    """
+    text = text.lower()
+    skips = [".", ",", '"', "'", ";", ":"]
+    for ch in skips:
+        text = text.replace(ch, "")
+    word_counts = {}
+    for word in text.split(" "):
+        if word in word_counts:
+            word_counts[word] += 1 
+        else:
+            word_counts[word] = 1
+    return word_counts
+
+print(count_words(text))
+
+def count_words_fast(text):
+    """
+    Count the number of times each word occurs in text(str). Return dictionary where keys are unique and values are word counts. Skip punctuation. For this function we have used Counter() from collection. 
+    """
+    text = text.lower()
+    skips = [".", ",", '"', "'", ";", ":"]
+    for ch in skips:
+        text = text.replace(ch, "")
+
+    word_counts = Counter(text.split(" "))
+    return word_counts
+
+print(count_words_fast(text))
+
+print(len(count_words_fast("This comprehension check is to check for comprehension.")))
+
+print(count_words(text) is count_words_fast(text)) # FALSE
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+def read_book(title_path):
+    """
+    Read a book and return it as a string.
+    """
+    with open(title_path, "r", encoding="utf8") as current_file:
+        text = current_file.read()
+        text = text.replace("\n", "").replace("\r", "")
+    return text
+
+ind = text.find("What's in a name?") # It prints the index position of this substring in text.
+
+sample_text = text[ind : ind + 1000]
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+def word_stats(word_counts):
+    """
+    Return number of unique words and word frequencies.
+    """
+    num_unique = len(word_counts)
+    counts = word_counts.values()
+    return (num_unique, counts)
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+import os
+
+book_dir = "C:/Users/User/Downloads/Books"
+
+import pandas as pd
+
+stats = pd.DataFrame(columns = ("language", "author", "title", "length", "unique"))
+title_num = 1
+for language in os.listdir(book_dir):
+    for author in os.listdir(book_dir + "/" + language):
+        for title in os.listdir(book_dir + "/" + language + "/" + author):
+            inputfile = book_dir + "/" + language + "/" + author + "/" + title
+            text = read_book(inputfile)
+            (num_unique, counts) = word_stats(count_words(text))
+            stats.loc[title_num] = language, author.capitalize(), title.replace(".txt", ""), sum(counts), num_unique
+            title_num += 1
+
+print(stats.head())
+
+
+
+
+
+
+
+
+
+
