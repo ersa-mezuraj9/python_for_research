824f020df95ede48a3cea546a47f15850f635b3c
ersa-mezuraj9 <ersamezuraj9@gmail.com>
2025-09-06 18:23:57 +0200

    Done with HW3-2

--- python_case_studies/language_processing/language_jpo.py
+++ python_case_studies/language_processing/language_jpo.py
@@ -83,12 +83,27 @@ for language in os.listdir(book_dir):
 
 print(stats.head())
 
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+import matplotlib.pyplot as plt
 
+plt.plot(stats.length, stats.unique, "bo")
+plt.show()
 
+plt.loglog(stats.length, stats.unique, "bo")
+plt.show()
 
-
-
-
-
+plt.figure(figsize = (10, 10))
+subset = stats[stats.language == "English"]
+plt.loglog(subset.length, subset.unique, "o", label = "English", color = "crimson")
+subset = stats[stats.language == "French"]
+plt.loglog(subset.length, subset.unique, "o", label = "French", color = "forestgreen")
+subset = stats[stats.language == "German"]
+plt.loglog(subset.length, subset.unique, "o", label = "German", color = "orange")
+subset = stats[stats.language == "Portuguese"]
+plt.loglog(subset.length, subset.unique, "o", label = "Portuguese", color = "blueviolet")
+plt.legend()
+plt.xlabel("Book Length")
+plt.ylabel("Number of unique words")
+plt.savefig("C:/Users/User/OneDrive/Desktop/python_for_research/python_case_studies/language_processing/lang_plot.pdf")
 
 
